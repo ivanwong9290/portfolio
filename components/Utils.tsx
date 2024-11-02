45c08651715c8ab8448b7d0c689a2fbeee59a77a
Ivan Wong <ivanwong9290@gmail.com>
2024-11-02 16:31:05 -0400

    Remove portfolio
    - Cleaning repo for restart

--- components/Utils.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-
-export const buildClassName = (classes: any[]): string => {
-    return classes.join(" ");
-}
-
-export const calcPagePercentage = (scrollHeight: number, maxHeight: number) => {
-    return scrollHeight / maxHeight * 100;
-}
