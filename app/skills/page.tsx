81cda97c45909faf75ec150dc803e8b9f2e5e6aa
Ivan Wong <ivanwong9290@gmail.com>
2024-11-02 21:11:19 -0400

    Placeholders for Projects & Skills page

---
+++ app/skills/page.tsx
@@ -0,0 +1,8 @@
+export default function Skills() {
+    return (
+        <section id="about">
+            <h2>About Us</h2>
+            <p>This is the about section.</p>
+        </section>
+    );
+}
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -6,8 +6,7 @@ import { SwitchProps, useSwitch } from "@nextui-org/switch";
 import { useTheme } from "next-themes";
 import { useIsSSR } from "@react-aria/ssr";
 import clsx from "clsx";
-
-import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
+import { Sun, Moon } from "lucide-react";
 
 export interface ThemeSwitchProps {
   className?: string;
@@ -70,11 +69,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
           ),
         })}
       >
-        {!isSelected || isSSR ? (
-          <SunFilledIcon size={22} />
-        ) : (
-          <MoonFilledIcon size={22} />
-        )}
+        {!isSelected || isSSR ? <Sun size={22} /> : <Moon size={22} />}
       </div>
     </Component>
   );
